c1d972725468e71a7a399654505d95d4e4c367e6
Stas Lebedenko <>
2022-01-27 14:20:44 +0200

    New group creation

--- MyStack.cs
+++ MyStack.cs
@@ -11,50 +11,61 @@ class MyStack : Stack
     public MyStack()
     {
         // Create an Azure Resource Group
-        var resourceGroup = new ResourceGroup("fancyGroup");
+        var resourceGroup = new ResourceGroup("fancyGroup", new ResourceGroupArgs
+        {
+            Location = "northeurope"
+        });
+
+        var groupWithRealName = new ResourceGroup("fancyGroupReal", new ResourceGroupArgs
+        {
+            Location = "northeurope",
+            ResourceGroupName = "fancyGroupReal"
+            //Name = "fancyGroupReal"
+        });
 
         // Create an Azure resource (Storage Account)
-        var storageAccount = new StorageAccount("fancyaccount", new StorageAccountArgs
-        {
-            ResourceGroupName = resourceGroup.Name,
-            Sku = new SkuArgs
-            {
-                Name = SkuName.Standard_LRS
-            },
-            Kind = Kind.StorageV2
-        });
+        // var storageAccount = new StorageAccount("fancyaccount", new StorageAccountArgs
+        // {
+        //     ResourceGroupName = resourceGroup.Name,
+        //     Location = "northeurope",
+        //     Sku = new SkuArgs
+        //     {
+        //         Name = SkuName.Standard_LRS
+        //     },
+        //     Kind = Kind.StorageV2
+        // });
 
         // Export the primary key of the Storage Account
         
-        this.PrimaryStorageKey = Output.Tuple(resourceGroup.Name, storageAccount.Name).Apply(names =>
-            Output.CreateSecret(GetStorageAccountPrimaryKey(names.Item1, names.Item2)));
+        // this.PrimaryStorageKey = Output.Tuple(resourceGroup.Name, storageAccount.Name).Apply(names =>
+        //     Output.CreateSecret(GetStorageAccountPrimaryKey(names.Item1, names.Item2)));
 
-        var appServicePlan = new AppServicePlan("festive-function-plan", new AppServicePlanArgs{
-            ResourceGroupName = resourceGroup.Name,
-            Kind = "Windows",
-            Sku = new  SkuDescriptionArgs {
-                Tier = "Dynamic",
-                Name = "Y1"
-            }
-        });
+        // var appServicePlan = new AppServicePlan("festive-function-plan", new AppServicePlanArgs{
+        //     ResourceGroupName = resourceGroup.Name,
+        //     Kind = "Windows",
+        //     Sku = new  SkuDescriptionArgs {
+        //         Tier = "Dynamic",
+        //         Name = "Y1"
+        //     }
+        // });
 
-        var app = new WebApp("festive-webapi-app", new WebAppArgs{
-            Kind = "FunctionApp",
-            ResourceGroupName = resourceGroup.Name,
-            ServerFarmId = appServicePlan.Id
-        });
+        // var app = new WebApp("festive-webapi-app", new WebAppArgs{
+        //     Kind = "FunctionApp",
+        //     ResourceGroupName = resourceGroup.Name,
+        //     ServerFarmId = appServicePlan.Id
+        // });
     }
 
-    [Output]
-    public Output<string> PrimaryStorageKey { get; set; }
+    // [Output]
+    // public Output<string> PrimaryStorageKey { get; set; }
 
-    private static async Task<string> GetStorageAccountPrimaryKey(string resourceGroupName, string accountName)
-    {
-        var accountKeys = await ListStorageAccountKeys.InvokeAsync(new ListStorageAccountKeysArgs
-        {
-            ResourceGroupName = resourceGroupName,
-            AccountName = accountName
-        });
-        return accountKeys.Keys[0].Value;
-    }
+    // private static async Task<string> GetStorageAccountPrimaryKey(string resourceGroupName, string accountName)
+    // {
+    //     var accountKeys = await ListStorageAccountKeys.InvokeAsync(new ListStorageAccountKeysArgs
+    //     {
+    //         ResourceGroupName = resourceGroupName,
+    //         AccountName = accountName
+    //     });
+    //     return accountKeys.Keys[0].Value;
+    // }
 }
